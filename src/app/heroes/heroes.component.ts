0734d098a6088ebe3d212b4e0445d89abedef402
pacifi <pacifi.bnr@gmail.com>
2020-08-30 10:07:52 -0500

    servicio de mensajes completado

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
 import {HeroService} from '../hero.service';
+import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,30 +11,24 @@ import {HeroService} from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes: Hero[];
   selectedHero: Hero;
 
+  heroes: Hero[];
 
-  constructor(private heroService: HeroService) {
-
+  constructor(private heroService: HeroService, private messageService: MessageService) {
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
-
-
   }
-
-
-  onSelect(hero: Hero): void {
-    console.log(hero);
-    this.selectedHero = hero;
-  }
-
-
 }
